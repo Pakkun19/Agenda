d90f2acb99b04172505d953fa94f2654ef6b4a52
pakkun19 <sanchezivan1919@gmail.com>
2023-02-26 20:04:03 +0100

    primer commit

---
+++ src/main/java/com/mycompany/agenda/Archivo.java
@@ -0,0 +1,40 @@
+package com.mycompany.agenda;
+
+import java.io.*;
+import javax.swing.JOptionPane;
+
+public class Archivo {
+    
+   File archivo;
+    
+    public void crearArchivo() {
+
+        try {
+
+            archivo = new File("AgendaContactos.txt");
+
+            if (archivo.createNewFile()) {
+                JOptionPane.showMessageDialog(null,"Archivo Creado");
+            }
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+
+    }
+    
+    public void escribirEnArchivo(Persona persona){
+        try{
+            FileWriter escritura = new FileWriter(archivo,true);
+            escritura.write(persona.getNombre() + "%" + persona.getCorreo() + "%" + persona.getTelefono() + "\r\n" );
+            escritura.close();
+        }catch(IOException e){
+            
+            System.out.println(e);
+        }
+        
+        
+    }
+    
+
+
+}
